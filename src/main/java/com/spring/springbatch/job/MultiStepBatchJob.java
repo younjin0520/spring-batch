efff174ffa71a05db4fa226c8925cd8b0d6b6657
younjin0520 <younjin0520@naver.com>
2023-04-02 02:37:58 +0900

    [spring-batch] multiJob 구현

---
+++ src/main/java/com/spring/springbatch/job/MultiStepBatchJob.java
@@ -0,0 +1,59 @@
+package com.spring.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class MultiStepBatchJob {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job ExampleJob(){
+        return jobBuilderFactory.get("multiStepJob")
+                .start(startStep())
+                .next(nextStep())
+                .next(lastStep())
+                .build();
+    }
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("MultiStepJob Start Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step nextStep(){
+        return stepBuilderFactory.get("nextStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("MultiStepJob Next Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step lastStep(){
+        return stepBuilderFactory.get("lastStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("MultiStepJob Last Step!!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
