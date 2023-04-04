1c6f4c8dd49b10f3fcf4cd4c57b2b4dd2de5d985
younjin0520 <younjin0520@naver.com>
2023-04-05 02:32:38 +0900

    [Step] skip 사용

---
+++ src/main/java/com/spring/springbatch/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.spring.springbatch.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class TestController {
+    private final Job StepTestJob;
+    private final JobLauncher jobLauncher;
+
+    @SneakyThrows
+    @GetMapping("/test/{number}")
+    public void test(@PathVariable Long number) {
+        log.info(number + "번째 실행");
+        jobLauncher.run(StepTestJob, new JobParameters());
+    }
+}
--- src/main/java/com/spring/springbatch/job/StepTestBatchJob.java
+++ src/main/java/com/spring/springbatch/job/StepTestBatchJob.java
@@ -1,23 +1,20 @@
 package com.spring.springbatch.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * StartLimit : 재시작 횟수 제한
- * 외부와의 통신, DB 작업 중 연결이 끊기면서 작업이 실패하는 경우, 배치를 재시작 해야함
- * -> 기본 재시작 가능 횟수 : 1회
- * -> startLimit()으로 횟수 지정 가능
- * -> 지정 횟수를 초과하면 Exception 발생
- */
+import java.sql.SQLException;
+
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -25,23 +22,125 @@ public class StepTestBatchJob {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+//    @Bean
+//    public Job startLimitJob() {
+//        return jobBuilderFactory.get("startLimitJob")
+//                .start(startLimitStep())
+//                .build();
+//    }
+
     @Bean
-    public Job startLimitJob() {
-        return jobBuilderFactory.get("startLimitJob")
-                .start(startLimitStep())
+    public Job skipJob() {
+        return jobBuilderFactory.get("skipJob")
+                .start(skipStep())
+                .build();
+    }
+    /**
+     * StartLimit : 재시작 횟수 제한
+     * 외부와의 통신, DB 작업 중 연결이 끊기면서 작업이 실패하는 경우, 배치를 재시작 해야함
+     * -> 기본 재시작 가능 횟수 : 1회
+     * -> startLimit()으로 횟수 지정 가능
+     * -> 지정 횟수를 초과하면 Exception 발생
+     */
+
+//    @Bean
+//    @JobScope
+//    public Step startLimitStep() {
+//        return stepBuilderFactory.get("startLimitStep")
+//                .startLimit(3)  // 재시작 3번 가능
+//                .tasklet((contribution, chunkContext) -> {
+//                    log.info(">>>> This is SimpleStep1");
+//                    int error = 5/0;    // 에러 발생 코드
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+
+    /* Skip: 데이터를 처리하는 동안 설정된 Exception이 발생한 경우, 해당 데이터 처리를 건너뛰는 기능 */
+    @Bean
+    public Step skipStep() {
+        return stepBuilderFactory.get("skipStep")
+                .<String, String>chunk(5)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .faultTolerant()
+                .skipLimit(4)
+                .skip(ArithmeticException.class)
+                .skip(SQLException.class)
+                .noSkip(NullPointerException.class)
                 .build();
     }
 
     @Bean
-    @JobScope
-    public Step startLimitStep() {
-        return stepBuilderFactory.get("startLimitStep")
-                .startLimit(3)  // 재시작 3번 가능
-                .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>> This is SimpleStep1");
-                    int error = 5/0;    // 에러 발생 코드
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
+    public ItemReader<String> reader() {
+        //데이터를 불러오는 로직을 작성
+        return new ItemReader<String>() {
+            int i = 0;
+
+            @SneakyThrows
+            @Override
+            public String read() throws ArithmeticException {
+                i++;
+
+                //skipLimit(2)이므로 아래 예외들은 모두 skip됨
+                if (i == 2) {
+                    log.error("ItemReader ArithmeticException 발생");
+                    throw new ArithmeticException("에러 발생");
+                }
+                if (i == 4) {
+                    log.error("ItemReader SQLException 발생");
+                    throw new SQLException("에러 발생");
+                }
+                log.info("itemReader >>> " + i);
+                return i > 20 ? null : String.valueOf(i);
+            }
+        };
+    }
+
+    @Bean
+    public ItemProcessor<String, String> processor() {
+        // 데이터를 처리하는 로직 작성
+        return item -> {
+            log.info("itemProcessor >>> " + item);
+
+            // 에러 발생 시, chunk의 첫 단계(1부터)로 돌아가 itemReader로부터 다시 데이터를 전달받음
+            // => 이미 에러가 발생한 item은 skip하고 넘어감
+            if(item.equals("3")){
+                log.error("ItemProcessor 에러 발생");
+                throw new SQLException();
+            }
+            return item;
+        };
+    }
+
+    @Bean
+    public ItemWriter<String> writer() {
+        // DB 저장과 같은 Transactional한 로직 작성
+        return items -> {
+            for (String item : items) {
+                if(item.equals("3")) {
+                    // 예외 발생 skip후에는 item을 리스트가 아니라 하나씩 받아옴 (처음에는 리스트로 받음)
+                    log.error("ItemWriter 에러 발생");
+                    throw new SQLException();
+                }
+            }
+            log.info("items >>> " + items);
+        };
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
